7ed319ff06592d0552ab14682a7cd9234f480863
Suellen Caroline da Silva <sucaroline5@gmail.com>
2021-09-08 17:31:58 -0300

    Write the state into the map

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from turtle import Screen
 import turtle
 import pandas
 
 
 # Create the screen
-screen = Screen()
+screen = turtle.Screen()
 screen.title("Name the States - U.S.")
 screen.setup(width=730, height=510)
 
@@ -15,17 +14,27 @@ turtle.shape(us_map)
 
 # Get the data
 data = pandas.read_csv("50_states.csv")
+state_list = data.state.tolist()
 
 # Set the Score
 correct_answers = 0
 total_states = len(data)
 
-# Prompt the user to type a State
-answer_state = screen.textinput(title=f"{correct_answers}/{total_states} - Guess the state", prompt="Type a state name.")
+# Use a loop to continuously guess
+while correct_answers < 50:
 
-# Check if the guess is correct
-state_list = data.state.tolist()
-for state in state_list:
-    if answer_state == state:
+    # Prompt the user to type a State
+    answer_state = screen.textinput(title=f"{correct_answers}/{total_states} - Guess the state", prompt="Type a state "
+                                                                                                        "name.")
+
+    # Check if the guess is correct
+    if answer_state in state_list:
+        state_data = data[data.state == answer_state]
+        # Write the state into the map
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        t.color("black")
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
         correct_answers += 1
-        print(correct_answers)
