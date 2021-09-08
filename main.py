9131ab2ea76ef4fe96c7d4481fc3485bf2c0328a
Suellen Caroline da Silva <sucaroline5@gmail.com>
2021-09-08 14:50:08 -0300

    Convert the guess to title case

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import turtle
 from turtle import Screen
+import turtle
+import pandas
+
 
 # Create the screen
 screen = Screen()
@@ -10,5 +12,15 @@ screen.setup(width=730, height=510)
 us_map = "blank_states_img.gif"
 screen.addshape(us_map)
 turtle.shape(us_map)
+
+# Get the data
+data = pandas.read_csv("50_states.csv")
+
+# Set the Score
+correct_answers = 0
+total_states = len(data)
+
+# Prompt the user to type a State
+answer_state = screen.textinput(title=f"{correct_answers}/{total_states} - Guess the state", prompt="Type a state name.").lower()
 
 
